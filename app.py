1a9d7c9475119304920e126a7669c699a4f1f455
sankaranarayanan <163542734+sankaranarayanansubbiah@users.noreply.github.com>
2024-04-13 13:27:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World! This is my simple web application.'
+    return 'Hello, World! This is my Flask web application running on EC2.'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Change the port number to your desired port
+    port_number = 9000
+    app.run(host='0.0.0.0', port=port_number)
